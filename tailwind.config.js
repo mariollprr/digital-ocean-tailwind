3b55bc52242e0c8ebfc0fb5b2b2528c86b4c4c76
Mario <alloparramario@gmail.com>
2022-10-12 16:51:24 +0200

    upload images, add slides section

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,14 @@ module.exports = {
         'do-blue-dark': 'rgb(8, 27, 75)',
         'do-blue-medium': 'rgb(0, 44, 155)',
         'do-blue-light': 'rgb(0, 105, 255)',
+        'do-blue-lighter': '#f9fafe'
       },
       boxShadow: {
         'input': '0 5px 1px 0 rgba(0, 0, 0, 0.1)',
         'input-focus': '0 2px 1px 0 rgba(0, 0, 0, 0.1)'
+      },
+      width: {
+        '88': '22rem'
       }
     },
   },
